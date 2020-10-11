d0b73ec4cd56873b965735a5615b1ce62ae3d4ed
ahmedatef00 <ahmedatef62437@gmail.com>
2020-10-11 08:01:53 +0200

    Refactor

--- resources/views/cart/show.blade.php
+++ resources/views/cart/show.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        
+
 
         <div class="container m-5">
             <table class="table table-sm">
@@ -26,11 +26,14 @@
                         <td>{{ $product->name }}</td>
                         <td>{{ $product->price }}</td>
                         <td>{{ $product->pivot->quantity }}</td>
-                        <td> {{ $product->price * $product->pivot->quantity}} </td>
+                        <td> {{ $product->price * $product->pivot->quantity}}
+                        </td>
+                        {{$sum += ($product->price * $product->pivot->quantity)}}
                         <td>
                             <form method="POST" action="{{route('cart.destroy',['product' => $product->id])}}">
                                 @csrf @method('delete')
-                                <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm('Are you sure that you want to delete this product ?')">
+                                <button type="submit" class="btn btn-danger btn-sm"
+                                    onclick="return confirm('Are you sure that you want to delete this product ?')">
                                     Delete </button>
                             </form>
                         </td>
@@ -41,16 +44,16 @@
                     @endif
                 </tbody>
             </table>
-    <h3>Total : ${{$sum}}</h3>
-    <form class="w3-container w3-display-middle w3-card-4 " method="POST" id="payment-form"
-                          action="{{ route('payment.paypal') }}">
-                        @csrf
-                        <p>
-                            <input type="text" class="w3-text-blue" name="amount" value="{{$sum}}" hidden/>
-                        </p>
-                        <button class="btn btn-primary" type="submit">Pay with PayPal</button>
-                        </p>
-                    </form>
+            <h3>Total : ${{$sum}}</h3>
+            <form class="w3-container w3-display-middle w3-card-4 " method="POST" id="payment-form"
+                action="{{ route('payment.paypal') }}">
+                @csrf
+                <p>
+                    <input type="text" class="w3-text-blue" name="amount" value="{{$sum}}" hidden />
+                </p>
+                <button class="btn btn-primary" type="submit">Pay with PayPal</button>
+                </p>
+            </form>
 
         </div>
 
